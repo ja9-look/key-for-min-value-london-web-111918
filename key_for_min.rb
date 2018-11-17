367890527c286914d800a9fb96eacda10ee7287a
Janine Luk <janineluk95@gmail.com>
2018-11-17 11:50:37 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,10 @@ def key_for_min_value(name_hash)
   min_value = nil
   min_key = nil
   name_hash.each do |k, v|
-    if min_value = nil || v <= min_value
+    if min_value == nil || v <= min_value
       min_value = v
       min_key = k
     end
-    ened
+  end
   min_key
 end
