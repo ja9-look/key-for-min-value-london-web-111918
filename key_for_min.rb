f1799996ed57a8b196fe25f27daa0ad4fd7e2cb1
Janine Luk <janineluk95@gmail.com>
2018-11-17 11:11:49 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,5 +5,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if value <= smallest
       smallest = value
-      
+      smallest_name = key
+    end
+    smallest_name
 end
